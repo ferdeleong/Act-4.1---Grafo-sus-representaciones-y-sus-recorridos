007d7aaf9304651732a2f67f6792b4ce10cd64c5
Fernanda De León <78885738+ferdeleong@users.noreply.github.com>
2022-05-30 20:15:21 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Act-41-Grafo-sus-representaciones-y-sus-recorridos/main.cpp
@@ -0,0 +1,21 @@
+/** 
+* Elena Ballinas García A01173324
+* Maria Fernanda De León A01197340
+
+Fecha de creacion: 30 de mayo del 2022
+
+* Ejemplo que implementa dos representaciones de Grafos
+* (Matriz de adyacencia y Lista de adyacencia) y el 
+* algoritmos BFS 
+*
+* Compilacion para debug:  
+*    g++ -std=c++17 -g -o main *.cpp 
+* Ejecucion con valgrind:
+*    nix-env -iA nixpkgs.valgrind
+*    valgrind --leak-check=full ./main < graph01.txt
+*
+* Compilacion para ejecucion:  
+*    g++ -std=c++17 -O3 -o main *.cpp 
+* Ejecucion:
+*    ./main < graph01.txt
+**/
